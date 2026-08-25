e4d829dffec06b1f2da1e9c2e0a8b122a83272ce
kuehnp <kuehnp@bzz.ch>
2026-08-25 10:45:43 +0200

    02a Konsoleneingabe

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,39 @@
 package ch.bzz;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class LibraryAppMain {
 
+    private static final Map<String, String> COMMANDS = new LinkedHashMap<>();
+    static {
+        COMMANDS.put("help", "shows this help text");
+        COMMANDS.put("quit", "exits the application");
+    }
+
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String s = scanner.next();
+            if (s.equals("quit")) {
+                break;
+            }
+            if (s.equals("help")) {
+                printHelp();
+            }
+            else {
+                System.out.println("Unknown command: " + s);
+            }
+
+        }
+        System.out.println("Exiting ...");
+        scanner.close();
+    }
+
+    private static void printHelp() {
+        System.out.println("Available commands:");
+        for (Map.Entry<String, String> command : COMMANDS.entrySet()) {
+            System.out.println("  " + command.getKey() + " - " + command.getValue());
+        }
     }
 }
